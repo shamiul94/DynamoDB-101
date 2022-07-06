90c0bdee3e0f36dbe919562a5b078b97ab84b3a1
Shamiul Hasan <hasan.shamiul@bkash.com>
2022-07-06 13:21:17 +0600

    Initialize project

---
+++ src/main/java/com/example/dynamodb101/DynamoDb101Application.java
@@ -0,0 +1,13 @@
+package com.example.dynamodb101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDb101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamoDb101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dynamodb101/DynamoDb101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamodb101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDb101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
